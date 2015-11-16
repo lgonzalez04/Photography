b348a46e2e88b63ee1c7a5719de63206e7ee3b58
lgonzalez04 <th_luis89@hotmail.com>
2015-11-16 12:42:18 -0500

    main

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,24 +1,24 @@
-// $(document).ready(function() {
-//
-//   $("#owl-demo").owlCarousel({
-//
-//       navigation : true, // Show next and prev buttons
-//       slideSpeed : 300,
-//       paginationSpeed : 400,
-//       singleItem:true,
-//       autoPlay:true,
-//       pagination:false,
-//
-//       // "singleItem:true" is a shortcut for:
-//       // items : 1,
-//       // itemsDesktop : false,
-//       // itemsDesktopSmall : false,
-//       // itemsTablet: false,
-//       // itemsMobile : false
-//
-//   });
-//
-// });
+$(document).ready(function() {
+
+  $("#owl-demo").owlCarousel({
+
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true,
+      autoPlay:true,
+      pagination:false,
+
+      // "singleItem:true" is a shortcut for:
+      // items : 1,
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+
+  });
+
+});
 
 
 @media only screen and (max-width: 767px) {}
